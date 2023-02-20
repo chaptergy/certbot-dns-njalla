08991de91d3b8a37b3341269c6497065970f2805
Grant West <grantwest2@gmail.com>
2023-02-19 20:50:23 -0600

    make description iterable
    The description field on the official dns plugin Authenticators are single element tuples: https://github.com/certbot/certbot/blob/master/certbot-dns-cloudflare/certbot_dns_cloudflare/_internal/dns_cloudflare.py#L26
    
    Fixes #5

--- certbot_dns_njalla/dns_njalla.py
+++ certbot_dns_njalla/dns_njalla.py
@@ -15,7 +15,7 @@ class Authenticator(dns_common.DNSAuthenticator):
     This Authenticator uses the Njalla REST API to fulfill a dns-01 challenge.
     """
 
-    description = "Obtain certificates using a DNS TXT record (if you are using Njalla for DNS)."
+    description = ("Obtain certificates using a DNS TXT record (if you are using Njalla for DNS).")
     ttl = 60
 
     def __init__(self, *args, **kwargs):
